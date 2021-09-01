03e798d07cf51dfb190136b31bc244a247ba61ed
asukhodko <asukhodko@gmail.com>
2021-09-01 17:46:09 +0300

    Add files via upload

---
+++ Beertown.Web/Code/PathFinder.cs
@@ -0,0 +1,58 @@
+﻿using Beertown.Domain;
+using Beertown.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Beertown.Web.Code
+{
+    public class PathFinder
+    {
+        public class Edge
+        {
+            public int NextNodeId { get; set; }
+            public decimal EdgeVeight { get; set; }
+            public decimal Distance { get; set; }
+        }
+
+        Dictionary<int, ISet<Edge>> _Graph;
+        Dictionary<int, Edge> _ClosestNodes;
+        public Dictionary<int, Edge> ClosedNodes { get { return _ClosestNodes; } }
+
+        public PathFinder(Dictionary<int, ISet<Edge>> graph, int startNode)
+        {
+            _Graph = graph;
+            _ClosestNodes = new Dictionary<int, Edge>();
+            FindPath(startNode);
+        }
+
+        private void FindPath(int startNode)
+        {
+            List<int> unprocessedNodesIdxs = new List<int>();
+            foreach(var n in _Graph.Keys)
+            {
+                _ClosestNodes[n] = new Edge { Distance = decimal.MaxValue };
+                unprocessedNodesIdxs.Add(n);
+            }
+            _ClosestNodes[startNode].Distance = 0;
+
+            while(unprocessedNodesIdxs.Count > 0)
+            {
+                var currentNodeIdx = unprocessedNodesIdxs.First(p => _ClosestNodes[p].Distance == unprocessedNodesIdxs.Min(pp => _ClosestNodes[pp].Distance));
+                unprocessedNodesIdxs.Remove(currentNodeIdx);
+
+                foreach(var outEdge in _Graph[currentNodeIdx])
+                {
+                    decimal currentDistance = _ClosestNodes[outEdge.NextNodeId].Distance;
+                    decimal newDistance = Math.Min(currentDistance, _ClosestNodes[currentNodeIdx].Distance + outEdge.EdgeVeight);
+                    if(newDistance < currentDistance)
+                    {
+                        _ClosestNodes[outEdge.NextNodeId].Distance = newDistance;
+                        _ClosestNodes[outEdge.NextNodeId].NextNodeId = currentNodeIdx;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Beertown.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Beertown.Web.MvcApplication" Language="C#" %>

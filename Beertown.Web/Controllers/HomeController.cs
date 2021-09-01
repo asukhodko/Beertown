16929b20039c0283d667b5ef6b6034b1f6a33b54
asukhodko <asukhodko@gmail.com>
2021-09-01 17:48:26 +0300

    Add files via upload

---
+++ Beertown.Web/Controllers/HomeController.cs
@@ -0,0 +1,106 @@
+﻿using Beertown.Domain;
+using Beertown.Web.Code;
+using Beertown.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Beertown.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        ISubwayRepository _SubwayRepository;
+
+        public HomeController(ISubwayRepository subwayRepository)
+        {
+            _SubwayRepository = subwayRepository;
+        }
+
+        public ActionResult Index()
+        {
+            var model = new BeertownModel();
+
+            InitLists(model);
+
+            return View(model);
+        }
+
+        private void InitLists(BeertownModel model)
+        {
+            List<SelectListGroup> groups = _SubwayRepository.Lines.Select(p => new SelectListGroup { Name = p.LineName }).ToList();
+            model.Stations = _SubwayRepository.Stations.Select(p => new SelectListItem { Value = p.StationId.ToString(), Text = p.StationName, Group = groups.First(pp => pp.Name == p.Line.LineName) });
+        }
+
+        private static void AddEdge(Dictionary<int, ISet<PathFinder.Edge>> graph, int stationId, int nextStationId, decimal duration)
+        {
+            PathFinder.Edge edge = new PathFinder.Edge { NextNodeId = nextStationId, EdgeVeight = duration };
+            graph[stationId].Add(edge);
+        }
+
+        protected Dictionary<int, ISet<PathFinder.Edge>> CreateGraph()
+        {
+            Dictionary<int, ISet<PathFinder.Edge>> graph = new Dictionary<int, ISet<PathFinder.Edge>>();
+
+            foreach (var s in _SubwayRepository.Stations)
+            {
+                graph[s.StationId] = new HashSet<PathFinder.Edge>();
+                foreach (var h in s.HaulsIn)
+                {
+                    AddEdge(graph, s.StationId, h.StationIn.StationId, h.DurationMins);
+                }
+                foreach (var h in s.HaulsOut)
+                {
+                    AddEdge(graph, s.StationId, h.StationOut.StationId, h.DurationMins);
+                }
+                foreach (var c in s.ConnectingPassagesIn)
+                {
+                    AddEdge(graph, s.StationId, c.StationIn.StationId, c.DurationMins);
+                }
+                foreach (var c in s.ConnectingPassagesOut)
+                {
+                    AddEdge(graph, s.StationId, c.StationOut.StationId, c.DurationMins);
+                }
+            }
+            return graph;
+        }
+
+        [HttpPost]
+        public ActionResult Index(BeertownModel model)
+        {
+            Dictionary<int, ISet<PathFinder.Edge>> graph = CreateGraph();
+
+            PathFinder pathFinder = new PathFinder(graph, model.StartStationId);
+
+            // Заполняем путь для отображения.
+            model.Path = new List<BeertownModel.PathNode>();
+            var toStation = _SubwayRepository.Stations.First(p => p.StationId == model.DestinationStationId);
+            while (toStation.StationId != model.StartStationId)
+            {
+                var closestNode = pathFinder.ClosedNodes[toStation.StationId];
+                var fromStation = _SubwayRepository.Stations.First(p => p.StationId == closestNode.NextNodeId);
+                model.Path.Insert(0, new BeertownModel.PathNode
+                {
+                    Station = new BeertownModel.SubwayStation { StationId = toStation.StationId, StationName = toStation.StationName, LineColorCode = toStation.Line.LineColorCode, LineId = toStation.LineId },
+                    Duration = closestNode.Distance
+                });
+                toStation = fromStation;
+            }
+            model.Path.Insert(0, new BeertownModel.PathNode
+            {
+                Station = new BeertownModel.SubwayStation { StationId = toStation.StationId, StationName = toStation.StationName, LineColorCode = toStation.Line.LineColorCode, LineId = toStation.LineId },
+                Duration = 0
+            });
+
+            model.Cost = new List<BeertownModel.CostDetail>();
+            foreach (var t in _SubwayRepository.LineTarifs.Where(p => p.ActualDate <= DateTime.Now && p.ActualEndDate >= DateTime.Now && model.Path.Select(pp => pp.Station.LineId).Contains(p.LineId)))
+            {
+                model.Cost.Add(new BeertownModel.CostDetail { Cost = t.Cost, CostItem = "проезд по " + t.Line.LineNameDativeCase + " линии", LineColorCode = t.Line.LineColorCode });
+            }
+
+            InitLists(model);
+            return View(model);
+        }
+    }
+}
---
+++ Beertown.Web/Models/BeertownModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Beertown.Web.Models
+{
+    public class BeertownModel
+    {
+        public class SubwayStation
+        {
+            public int StationId { get; set; }
+            public string StationName { get; set; }
+            public int LineId { get; set; }
+            public string LineColorCode { get; set; }
+        }
+
+        public class PathNode
+        {
+            public SubwayStation Station { get; set; }
+            public decimal Duration { get; set; }
+        }
+
+        public class CostDetail
+        {
+            public string CostItem { get; set; }
+            public decimal Cost { get; set; }
+            public string LineColorCode { get; set; }
+        }
+
+        [DisplayName("Начальная станция")]
+        public int StartStationId { get; set; }
+
+        [DisplayName("Конечная станция")]
+        public int DestinationStationId { get; set; }
+
+        public IEnumerable<SelectListItem> Stations { get; set; }
+
+        public IList<PathNode> Path { get; set; }
+
+        public IList<CostDetail> Cost { get; set; }
+    }
+}
